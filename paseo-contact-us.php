f2c8ca74cbf6c1201def58d6b14237bed5c7f585
Johan Martin <martin.johan@johan-martin.com>
2017-09-22 19:50:53 +0200

    update with basic plugin info

--- paseo-contact-us.php
+++ paseo-contact-us.php
@@ -32,88 +32,27 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-define( 'PLUGIN_VERSION', '1.0.0' );
+global $wp_version;
 
-add_action('init', 'register_wp_questions');
-function registers_wp_questions() {
-		$wpwa_questions = 'wpwa_questions';
-	$labels = array(
-		'name' => __( 'Questions', $wpwa_questions ),
-		'singular_name' => __( 'Add New Question', $wpwa_questions ),
-		'add_new' => __( 'Add New', $wpwa_questions ),
-		'add_new_item' => __('Add New Question', $wpwa_questions ),
-		'edit_item' => __( 'Edit Questions', $wpwa_questions ),
-		'new_item' => __( 'New Question', $wpwa_questions ),
-		'view_item' => __( 'View Question', $wpwa_questions ),
-		'search_items' => __( 'Search Questions', $wpwa_questions ),
-		'not_found' => __( 'No Questions Found', $wpwa_questions ),
-		'not_found_in_trash' => __( 'No Questions found in Trash', $wpwa_questions ),
-		'parent_item_colon' => __( 'Parent Questins: ', $wpwa_questions ),
-		'menu_name' => __( 'Questions', $wpwa_questions ),
-	);
-	$args = array(
-		'label' => $labels,
-		'hierarchical' => true,
-		'description' => __( 'Questions and Answers', $wpwa_questions ),
-		'supports' => array( 'title', 'editor', 'comments' ),
-		'public' => true,
-		'show_ui' => true,
-		'show_in_menu' => true,
-		'show_in_nav_menus' => true,
-		'publicly_queryable' => true,
-		'exclude_from_search' => false,
-		'has_archive' => true,
-		'query_var' => true,
-		'can_export' => true,
-		'rewrite' => true,
-		'capability_type' => 'post'
-	);
+$exit_message = 'Requires Wordpress 4.8 or newer';
 
-	register_post_type( $wpwa_questions, $args );
+if (version_compare($wp_version, "4.7", "<")){
+	exit( $exit_message );
 }
 
+define( 'PLUGIN_VERSION', '0.0.1' );
 
+add_action( 'rest_api_init', 'paseo_register_route');
 
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-paseo-contact-us-activator.php
- */
-// function activate_paseo_contact_us() {
-// 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-paseo-contact-us-activator.php';
-// 	Paseo_Contact_Us_Activator::activate();
-// }
+function paseo_contact_form( WP_REST_Request $request ) {
+	return $request->get_params();
+}
 
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-paseo-contact-us-deactivator.php
- */
-// function deactivate_paseo_contact_us() {
-// 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-paseo-contact-us-deactivator.php';
-// 	Paseo_Contact_Us_Deactivator::deactivate();
-// }
-
-// register_activation_hook( __FILE__, 'activate_paseo_contact_us' );
-// register_deactivation_hook( __FILE__, 'deactivate_paseo_contact_us' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-// require plugin_dir_path( __FILE__ ) . 'includes/class-paseo-contact-us.php';
-
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-// function run_paseo_contact_us() {
-
-// 	$plugin = new Paseo_Contact_Us();
-// 	$plugin->run();
-
-// }
-// run_paseo_contact_us();
+function paseo_register_route() {
+	register_rest_route('paseo/v1', '/author/(?P<id>\d+)',
+	array(
+		'methods' => 'GET',
+		'callback' => 'paseo_contact_form'
+	)
+	);
+}

c064fa0ef35d9da2ab98fa3215f03faeee0bfd53
Johan Martin <martin.johan@johan-martin.com>
2017-09-22 22:28:25 +0200

    update paseo contact

--- paseo-contact-us.php
+++ paseo-contact-us.php
@@ -45,14 +45,57 @@ define( 'PLUGIN_VERSION', '0.0.1' );
 add_action( 'rest_api_init', 'paseo_register_route');
 
 function paseo_contact_form( WP_REST_Request $request ) {
-	return $request->get_params();
+	$id = $request->get_param('id');
+	$post = get_post_meta( $id, "wporg_meta_key" );
+	return $post;
 }
 
 function paseo_register_route() {
-	register_rest_route('paseo/v1', '/author/(?P<id>\d+)',
+	register_rest_route('paseo/v1', '/post/(?P<id>\d+)',
 	array(
 		'methods' => 'GET',
 		'callback' => 'paseo_contact_form'
 	)
 	);
 }
+
+/* Add Meta Boxes */
+function paseo_contact_form_custom_box() {
+	$screens = ['post', 'wporg_cpt'];
+	foreach( $screens as $screen ) {
+		add_meta_box(
+			'paseo_box_id',
+			'Custom Meta Box',
+			'wporg_custom_box_html',
+			$screen
+		);
+	}
+}
+
+add_action('add_meta_boxes', 'paseo_contact_form_custom_box');
+
+function wporg_custom_box_html( $post ) {
+	?>
+	<label for="wporg_field">Description for this field</label>
+	<select name="wporg_field" id="wporg_field" class="postbox">
+		<option value="">Select something...</option>
+		<option value="something">Something</option>
+		<option value="else">Else</option>
+		<option value="totall">Totally random</option>
+	</select>
+	<?php
+}
+
+function paseo_contact_form_save_meta( $post_id ) {
+	if (array_key_exists('wporg_field', $_POST)) {
+		update_post_meta(
+			$post_id,
+			'wporg_meta_key',
+			$_POST['wporg_field']
+		);
+	}
+}
+
+
+add_action('save_post', 'paseo_contact_form_save_meta');
+

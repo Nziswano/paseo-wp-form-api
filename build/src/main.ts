9feb5b43b65a99e13dd808313dc683c5a6b21601
Johan Martin <martin.johan@johan-martin.com>
2017-10-21 00:36:02 +0200

    build for js and css

---
+++ build/src/main.ts
@@ -0,0 +1,11 @@
+import $ from "jquery";
+import jQuery from "jquery";
+
+import "./scss/app.scss";
+
+
+jQuery( document ).ready( function( $ ) {
+  
+    // Your JavaScript goes here
+  
+  });
---
+++ build/webpack.config.js
@@ -0,0 +1,81 @@
+const path = require('path')
+const webpack = require('webpack')
+const CleanWebPackPlugin = require('clean-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  entry: './src/main.ts',
+  output: {
+    path: path.resolve(__dirname, './static/assets'),
+    filename: 'app.js'
+  },
+  plugins: [
+    new CleanWebPackPlugin(['./static/assets']),
+    new ExtractTextPlugin({
+      filename: 'styles.css',
+      allChunks: true
+    })
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            'postcss-loader',
+            'sass-loader'
+          ]
+        })
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.ts$/,
+        loader: 'awesome-typescript-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'file-loader',
+        options: {
+          limit: 10000
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      }
+    ]
+  },
+  devtool: '#source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}

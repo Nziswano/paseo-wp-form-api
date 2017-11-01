e88918a79b4ed7b5d9a5cf6ab4c8cb9cef9da687
Johan Martin <martin.johan@johan-martin.com>
2017-11-01 02:21:14 +0200

    added rest routing and able to retrieve data from database

--- src/Admin/Listings/Listings.php
+++ src/Admin/Listings/Listings.php
@@ -11,15 +11,25 @@ namespace Paseo\Admin\Listings;
 
 class Listings
 {
+  /**
+   * @var \wpdb
+   */
   private $db;
 
+  /**
+   * @var string
+   */
   private $table_name;
+
+  /**
+   * @var singleton
+   */
+  private static $listing = null;
 
   /**
    * Listings constructor.
    */
-  public function __construct()
-  {
+  private function __construct() {
     global $wpdb;
     $this->db = $wpdb;
     $this->table_name = $wpdb->prefix . \DB_TABLE;
@@ -40,20 +50,45 @@ class Listings
    * @return mixed
    */
   public function getContact( $contact_id ) {
-
+    $query = "SELECT * FROM " . $this->table_name . " WHERE id = %d";
+    $contact = $this->db->get_row( $this->db->prepare( $query, $contact_id));
     return $contact;
   }
+
+  /**
+   * @param $contact_id
+   * @return false|int
+   */
+  public function updateContact( $contact_id ) {
+
+    $result = $this->db->update(
+      $this->table_name,
+      array(
+        'is_processed' => 1
+      ),
+      array( 'id' => $contact_id),
+    array( '%d' ),
+      array('%')
+    );
+
+    return $result;
+  }
+
+  /**
+   * Process rest request
+   * @param \WP_REST_Request $request
+   * @return \WP_REST_Response
+   */
+  public static function process_listings( \WP_REST_Request $request ) {
+    $result = new \WP_REST_Response();
+    if( self::$listing == NULL ) {
+      self::$listing = new Listings();
+    }
+
+    if ( GET == $request->get_method() ) {
+      $result->set_data( self::$listing->getList());
+    }
+
+    return $result;
+  }
 }
-
-
-/**
- * $charset_collate = $wpdb->get_charset_collate();
-$table_name = $wpdb->prefix . DB_TABLE;
-
-$sql = "CREATE TABLE $table_name (
-id mediumint(9) NOT NULL AUTO_INCREMENT,
-fingerprint VARCHAR(75) NOT NULL,
-contact_info JSON,
-created TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-UNIQUE KEY id (id)
- */
--- src/Rest/Routes.php
+++ src/Rest/Routes.php
@@ -9,40 +9,48 @@
 namespace Paseo\Rest;
 
 
+class Routes
+{
+  public static function register_route()
+  {
+    register_rest_route(ROUTE, '/contact-us/',
+      array(
+        'methods' => 'GET, POST',
+        'callback' => array('Paseo\Contact\Contact', 'contact_form')
+      )
+    );
 
-class Routes {
-	public static function register_route() {
-		register_rest_route(ROUTE, '/contact-us/',
-			array(
-				'methods' => 'GET, POST',
-				'callback' => array('Paseo\Contact\Contact', 'contact_form')
-			)
-		);
+    register_rest_route(\ROUTE, '/settings',
+      array(
+        'methods' => 'POST',
+        'callback' => array('Paseo\Admin\Settings\Settings', 'update_settings'),
+        'permissions_callback' => array('Paseo\Admin\Settings\Settings', 'permissions')
+      )
+    );
+    register_rest_route(ROUTE, '/settings',
+      array(
+        'methods' => 'GET',
+        'callback' => array('Paseo\Admin\Settings\Settings', 'check_settings'),
+        'permissions_callback' => array('Paseo\Admin\Settings\Settings', 'permissions')
+      )
+    );
+    register_rest_route(\ROUTE, '/contacts',
+      array(
+        'methods' => 'GET, POST',
+        'callback' => array('Paseo\Admin\Listinngs\Listings', 'process_listings'),
+        'permissions_callback' => array('Paseo\Admin\Settings\Settings', 'permissions')
+      )
+    );
 
-        register_rest_route( \ROUTE, '/settings',
-            array(
-                'methods'         => 'POST',
-                'callback'        => array( 'Paseo\Admin\Settings\Settings', 'update_settings' ),
-                'permissions_callback' => array( 'Paseo\Admin\Settings\Settings', 'permissions' )
-            )
-        );
-        register_rest_route( ROUTE, '/settings',
-            array(
-                'methods'         => 'GET',
-                'callback'        => array( 'Paseo\Admin\Settings\Settings', 'check_settings' ),
-                'permissions_callback' => array( 'Paseo\Admin\Settings\Settings', 'permissions' )
-            )
-        );
-
-        /* Set header information */
-        remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
-        add_filter( 'rest_pre_serve_request', function( $value ) {
-            header( 'Access-Control-Allow-Origin: *' );
-            header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE' );
-            header( 'Access-Control-Allow-Credentials: true' );
-            header( 'Access-Control-Expose-Headers: X-WP-Total, X-WP-TotalPages, PAS-Fingerprint, PAS-Check, PAS-Nonce, PAS-Captcha');
-            header('Access-Control-Allow-Headers: Authorization, Content-Type, PAS-Nonce, PAS-Fingerprint, PAS-Check');
-            return $value;
-        });
-	}
+    /* Set header information */
+    remove_filter('rest_pre_serve_request', 'rest_send_cors_headers');
+    add_filter('rest_pre_serve_request', function ($value) {
+      header('Access-Control-Allow-Origin: *');
+      header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+      header('Access-Control-Allow-Credentials: true');
+      header('Access-Control-Expose-Headers: X-WP-Total, X-WP-TotalPages, PAS-Fingerprint, PAS-Check, PAS-Nonce, PAS-Captcha');
+      header('Access-Control-Allow-Headers: Authorization, Content-Type, PAS-Nonce, PAS-Fingerprint, PAS-Check');
+      return $value;
+    });
+  }
 }
